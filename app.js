e55c6fcf1956ce419174f16393965291730f2ce8
EDLyonhart <EDLyonhart@gmail.com>
2015-01-29 09:26:38 -0800

    basics working, all the basics now.

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ $(document).ready(function (){
 		event.preventDefault();
 		var movie = $("#movie").val();	
 		$.getJSON("http://www.omdbapi.com/?s=" + movie, function(data){
-			if (data.Search.length > 0) {
+			if (data.Error) {
+				$("ol").append("<li>Sorry brah, no results</li>");
+			} else {
 				for (var value in data.Search){
 					$("ol").append("<li>" + (data.Search[value].Title) + "</li>");
 				}
