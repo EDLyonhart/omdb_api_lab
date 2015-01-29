ca6beb361159bb3ef68528f8b99455ba0d7c1741
EDLyonhart <EDLyonhart@gmail.com>
2015-01-29 09:46:08 -0800

    no bullshit. this tyme i actually finished all the assignment requirements

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-$(document).ready(function (){
+$(document).ready(function (){														//wait til the doc is loaded to run JavaScript
 
-	$("form").on("submit", function(event) {
-		event.preventDefault();
-		var movie = $("#movie").val();	
-		$.getJSON("http://www.omdbapi.com/?s=" + movie, function(data){
-			if (data.Error) {
-				$("ol").append("<li>Sorry brah, no results</li>");
+	$("form").on("submit", function(event) {										//listen for a submit event
+		event.preventDefault();														//keep page from default reloading
+		$("li").remove();															//clear previous entries
+		var movie = $("#movie").val();												//grab #movie id in a variable
+		$.getJSON("http://www.omdbapi.com/?s=" + movie, function(data){				//getJSON and create the full URL
+			if (data.Error) {														//if there are no results:
+				$("ol").append("<li>Sorry brah, no results</li>");						//throw an error message
 			} else {
-				for (var value in data.Search){
-					$("ol").append("<li>" + (data.Search[value].Title) + "</li>");
+				for (var value in data.Search){										//otherwise, if there are results
+					$("ol").append("<li>" + (data.Search[value].Title) + "</li>");		//append the title into the <li>st
 				}
 			}
 		});
